bf7cd3a5466b11c4b114668c902510ccbd596188
adityaspande1 <121101849+adityaspande1@users.noreply.github.com>
2025-03-25 20:17:34 +0530

    basic types added for graph

---
+++ src/types/graph.ts
@@ -0,0 +1,25 @@
+export interface Node{
+    id:string,
+    name: string,
+    type: string,
+    language: string,
+    filepath?: string,
+    metadata?: Record<string,any>;
+}
+
+export interface Edge{
+    source: string,
+    target: string,
+    type: string,
+    metadata?: Record<string,any>;
+}
+
+export interface GraphData{
+    projectName: string,
+    langauge: string,
+    version: string,
+    analyzedAt: string,
+    nodes: Node[],
+    edges: Edge[],
+    metadata?: Record<string,any>;
+}
